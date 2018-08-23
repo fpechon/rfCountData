88c040ed37a20db03a390def8b4b8e9c159b104a
Florian Pechon <pechonf@oasis.uclouvain.be>
2018-08-23 10:52:15 +0200

    add help file with roxygen comments

--- R/treesize.R
+++ R/treesize.R
@@ -1,5 +1,9 @@
 #' treesize
-#' 
+#' @param x, an object of class \code{rfCountData}, which contains a \code{forest} component.
+#' @param terminal, count terminal nodes only (\code{TRUE}) or all nodes (\code{FALSE}
+#' @return A vector containing number of nodes for the trees in the \code{rfCountData} object.
+#' @note The \code{rfCountData} object must contain the \code{forest} component; i.e., created with \code{rfPoisson(...,keep.forest=TRUE)}.
+#' @author Andy Liaw \email{andy_liaw@merck.com}
 #' @export
 treesize <- function(x, terminal=TRUE) {
   if(!inherits(x, "rfCountData"))
